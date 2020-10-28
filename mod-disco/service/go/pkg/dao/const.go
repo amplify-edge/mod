05d42505e6064199b5d522647c15b3c1044879e2
gutterbacon <gutterbacon@protonmail.com>
2020-10-28 10:22:40 +0700

    mod-disco skeleton

---
+++ mod-disco/service/go/pkg/dao/const.go
@@ -0,0 +1,32 @@
+package dao
+
+import (
+	"strings"
+)
+
+// const.go contains
+// all the table columns / fields basically.
+const (
+	SurveyProjectTableName   = "survey_projects"
+	SurveyUsersTableName     = "survey_users"
+	SurveyProjectColumns     = `survey_project_id, sys_account_project_ref_id, survey_schema_types, survey_filter_types, created_at, updated_at`
+	SurveyProjectColumnsType = `TEXT, TEXT, BLOB, BLOB, INT, INT`
+	SurveyUsersColumns       = `survey_user_id, survey_project_ref_id, sys_account_account_ref_id, survey_schema_values, survey_schema_filters, created_at, updated_at`
+	SurveyUsersColumnsType   = ``
+	DefaultLimit             = 50
+	DefaultCursor            = `created_at`
+)
+
+// initFields will only be called once during SurveyProjectountDB initialization (singleton)
+func initFields(columns string, values string) map[string]string {
+	ret := map[string]string{}
+	vals := strings.Split(values, ",")
+	for i, col := range strings.Split(columns, ",") {
+		if i == 0 {
+			ret[col] = vals[i] + " PRIMARY KEY"
+		} else {
+			ret[col] = vals[i]
+		}
+	}
+	return ret
+}
