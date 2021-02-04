e89ca9d4cf326e087a3fab6f5defe8fb7d9e1a45
gutterbacon <gutterbacon@protonmail.com>
2021-02-04 08:57:14 +0000

    fixing authz enforcer

--- mod-disco/service/go/pkg/repo/authz_enforcer.go
+++ mod-disco/service/go/pkg/repo/authz_enforcer.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"encoding/json"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -11,28 +12,21 @@ import (
 
 // permission check using in-process messaging
 type sysAllowInput struct {
-	eventName string `json:"event_name"`
-	orgId     string `json:"org_id"`
-	projectId string `json:"project_id"`
-	userId    string `json:"user_id"`
+	EventName string `json:"-"`
+	OrgId     string `json:"org_id,omitempty"`
+	ProjectId string `json:"project_id,omitempty"`
+	UserId    string `json:"user_id,omitempty"`
 }
 
 // checkAllow checks for permission from the sys-account via the in-process messaging
 // if allowed, it will proceed.
 func (md *ModDiscoRepo) checkAllow(ctx context.Context, in *sysAllowInput) (bool, error) {
-	params := map[string]interface{}{}
-	if in.orgId != "" {
-		params[in.orgId] = in.orgId
-	}
-	if in.projectId != "" {
-		params[in.projectId] = in.projectId
-	}
-	payloadBytes, err := sysCoreSvc.MarshalToBytes(params)
+	payloadBytes, err := json.Marshal(in)
 	if err != nil {
 		return false, err
 	}
 	resp, err := md.busClient.Broadcast(ctx, &corepkg.EventRequest{
-		EventName:   in.eventName,
+		EventName:   in.EventName,
 		Initiator:   moduleName,
 		JsonPayload: payloadBytes,
 	})
@@ -53,9 +47,9 @@ func (md *ModDiscoRepo) checkAllow(ctx context.Context, in *sysAllowInput) (bool
 // and the project admin itself.
 func (md *ModDiscoRepo) allowDiscoProject(ctx context.Context, orgId, projectId string) bool {
 	in := &sysAllowInput{
-		eventName: "onCheckAllowProject",
-		orgId:     orgId,
-		projectId: projectId,
+		EventName: "onCheckAllowProject",
+		OrgId:     orgId,
+		ProjectId: projectId,
 	}
 	allowed, err := md.checkAllow(ctx, in)
 	if err != nil {
@@ -68,8 +62,8 @@ func (md *ModDiscoRepo) allowDiscoProject(ctx context.Context, orgId, projectId 
 // SurveyUser update and delete should only be available for either superuser or its own user.
 func (md *ModDiscoRepo) allowSurveyUser(ctx context.Context, userId string) bool {
 	in := &sysAllowInput{
-		eventName: "onCheckAllowSurveyUser",
-		userId:    userId,
+		EventName: "onCheckAllowSurveyUser",
+		UserId:    userId,
 	}
 	allowed, err := md.checkAllow(ctx, in)
 	if err != nil {
