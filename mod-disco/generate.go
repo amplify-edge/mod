f6b40de9041a946b7b0415366d5031dc8e6bf3ea
gutterbacon <gutterbacon@protonmail.com>
2021-03-01 13:47:38 +0000

    TODO: refactor mod

--- mod-disco/generate.go
+++ mod-disco/generate.go
@@ -10,14 +10,13 @@ package main
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'Generating protobuf and grpc services golang'"
-//go:generate protoc -I./proto/v2/ -I. --go_out=./service/go/rpc/v2 --go_opt=paths=source_relative ./proto/v2/mod_disco_models.proto
-//go:generate protoc -I./proto/v2/ -I. --go-grpc_out=./service/go/rpc/v2/ --cobra_out=./service/go/rpc/v2 --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/mod_disco_services.proto
+//go:generate booty protoc -I./proto/v2/ -I. --go_out=./service/go/rpc/v2 --go_opt=paths=source_relative ./proto/v2/mod_disco_models.proto
+//go:generate booty protoc -I./proto/v2/ -I. --go-grpc_out=./service/go/rpc/v2/ --cobra_out=./service/go/rpc/v2 --go-grpc_opt=paths=source_relative --cobra_opt=paths=source_relative ./proto/v2/mod_disco_services.proto
 //go:generate protoc-go-inject-tag -input ./service/go/rpc/v2/mod_disco_models.pb.go
-
 
 // ============================================================================
 // Flutter
 // ============================================================================
 // GRPC & Protobuf
 //go:generate /usr/bin/env bash -c "echo 'generating protobuf and grpc services for flutter/dart'"
-//go:generate protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/rpc/v2/ ./proto/v2/mod_disco_models.proto ./proto/v2/mod_disco_services.proto
+//go:generate booty protoc -I./proto/v2/ -I. --dart_out=grpc:./service/flu/lib/rpc/v2/ ./proto/v2/mod_disco_models.proto ./proto/v2/mod_disco_services.proto
